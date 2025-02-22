04daeb2afeae5e56dcdc9323185ad2c08c6cf9c8
ZHA Wen Ting <wentingzha881030@gmail.com>
2025-02-21 22:14:13 -0500

    Develop Back End for Registration

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -40,10 +40,10 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceRoutes);
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/auth', authRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/secondchance/search', searchRoutes);
